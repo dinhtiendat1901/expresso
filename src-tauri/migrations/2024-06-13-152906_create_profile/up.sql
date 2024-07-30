c20939f0cf5a284563b8145388db689b29fadeff
dinhtiendat190196 <dinhtiendat1901@gmail.com>
2024-07-30 15:26:00 +0700

    update SQL

--- src-tauri/migrations/2024-06-13-152906_create_profile/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE profile
-(
-    id           INTEGER PRIMARY KEY AUTOINCREMENT,
-    name         VARCHAR(100),
-    description  VARCHAR(100),
-    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
