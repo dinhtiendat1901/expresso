b94058705c63003b35089f37a6473ad682684edc
dinhtiendat1901 <dinhtiendat1901@gmail.com>
2024-10-23 14:43:55 +0700

    control external module

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,77 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::process::{Command, Stdio};
+use std::os::windows::process::CommandExt;
+use std::process::{Child, Command, Stdio};
+use std::sync::{Arc, Mutex};
 
 mod commands;
 mod db;
 mod repositories;
 mod services;
 
+
+const CREATE_NO_WINDOW: u32 = 0x08000000;
+
+// Struct to manage a generic external process
+struct ExternalProcess {
+    process: Option<Child>,
+}
+
+impl ExternalProcess {
+    fn new(command: &str, args: &[&str]) -> Self {
+        let process = Command::new(command)
+            .args(args)
+            .creation_flags(CREATE_NO_WINDOW)
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+            .expect(&format!("Failed to start {}", command));
+
+        ExternalProcess { process: Some(process) }
+    }
+
+    fn kill(&mut self) {
+        if let Some(mut process) = self.process.take() {
+            match process.kill() {
+                Ok(_) => {
+                    println!("Process killed.");
+                    let _ = process.wait(); // Ensure process has been terminated
+                }
+                Err(e) => eprintln!("Failed to kill process: {}", e),
+            }
+        }
+    }
+}
+
+
 fn main() {
-    // Command::new("./binaries/puppeteer-server-win.exe")
-    //     .stdout(Stdio::null())
-    //     .stderr(Stdio::null())
-    //     .spawn()
-    //     .expect("Failed to start external module");
-    tauri::Builder::default()
+    let puppeteer_server = Arc::new(Mutex::new(ExternalProcess::new(
+        "./binaries/puppeteer-server-win.exe",
+        &[],
+    )));
+
+    let redis_server = Arc::new(Mutex::new(ExternalProcess::new(
+        "./binaries/Redis-7.4.1-Windows-x64-cygwin/redis-server.exe",
+        &["--port", "6379"],
+    )));
+
+    let puppeteer_server_clone = Arc::clone(&puppeteer_server);
+    let redis_server_clone = Arc::clone(&redis_server);
+
+    tauri::Builder::default().on_window_event(move |_window, event| {
+        if let tauri::WindowEvent::CloseRequested { .. } = event {
+            println!("Window close requested. Killing external processes...");
+            {
+                let mut server = puppeteer_server_clone.lock().unwrap();
+                server.kill();
+            }
+            {
+                let mut server = redis_server_clone.lock().unwrap();
+                server.kill();
+            }
+        }
+    })
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
